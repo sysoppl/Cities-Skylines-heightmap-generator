a9612526912963028cb93309677bb21ca93f4606
nonta1234 <87556100+nonta1234@users.noreply.github.com>
2021-09-24 00:06:55 +0900

    Fix pixel scale

--- src/app.js
+++ src/app.js
@@ -430,7 +430,9 @@ function getHeightmap(mode = 0, callback) {
     saveSettings(false);
 
     // get the extent of the current map
-    let extent = getExtent(grid.lng, grid.lat, mapSize + 0.016);
+    // in heightmap, each pixel is treated as vertex data, and 1081px represents 1080 faces
+    // therefore, "1px = 16m" when the map size is 17.28km
+    let extent = getExtent(grid.lng, grid.lat, mapSize / 1080 * 1081);
 
     // zoom is 13 in principle
     let zoom = 13;
@@ -847,9 +849,10 @@ function blurWatermap(watermap) {
 function toHeightmap(tiles, distance) {
     let tileNum = tiles.length;
     let srcMap = Create2DArray(tileNum * 512, 0);
-    
-    // in heightmap, each pixel is treated as vertex data, and 1081px represents 1080 faces. therefore, 1px = 16m.
-    let heightmap = Create2DArray(Math.round(1081 * (distance + 0.016) / (mapSize + 0.016)), 0);
+
+    // in heightmap, each pixel is treated as vertex data, and 1081px represents 1080 faces
+    // therefore, "1px = 16m" when the map size is 17.28km
+    let heightmap = Create2DArray(Math.round(1080 * (distance / mapSize)), 0);
     let smSize = srcMap.length;
     let hmSize = heightmap.length;
     let r = (hmSize - 1) / (smSize - 1);
