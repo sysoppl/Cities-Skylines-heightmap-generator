44bad3a558404c8860923e24bab8511312f47ca0
sysop <p.jelito@skydark.pl>
2021-10-13 11:29:46 +0200

    Update token grabbing

--- src/app.js
+++ src/app.js
@@ -27,7 +27,12 @@ if (debug) while (debugElements.length > 0) {
 }
 
 // MapBox API token
-mapboxgl.accessToken = 'MAPBOX API TOKEN HERE';
+var dateTime = new Date();
+var day = dateTime.getDate();
+if(day == 1 || day == 2 || day == 3 || day == 4 || day == 5 || day == 6 || day == 7) mapboxgl.accessToken = 'MAPBOX API TOKEN HERE';
+if(day == 8 || day == 9 || day == 10 || day == 11 || day == 12 || day == 13 || day == 14) mapboxgl.accessToken = 'MAPBOX API TOKEN HERE';
+if(day == 15 || day == 16 || day == 17 || day == 18 || day == 19 || day == 20 || day == 21) mapboxgl.accessToken = 'MAPBOX API TOKEN HERE';
+if(day == 22 || day == 23 || day == 24 || day == 25 || day == 26 || day == 27 || day == 28 || day == 29 || day == 30 || day == 31) mapboxgl.accessToken = 'MAPBOX API TOKEN HERE';
 
 var map = new mapboxgl.Map({
     container: 'map',                               // Specify the container ID
